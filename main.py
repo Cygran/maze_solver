e7a512dd0801584899f35803f1d790461cde5b83
Cygran <apcanning90@gmail.com>
2025-02-15 21:02:31 +0000

    Add line drawing functionality to MazeWindow

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from window import MazeWindow
+from window import MazeWindow, Point, Line
 
 def main():
     win = MazeWindow(800, 600)
+    l = Line(Point(100, 100), Point(200, 200))
+    win.draw_line(l, "black")
+    
     win.wait_for_close()
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -4,8 +4,8 @@ class MazeWindow:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__root.canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__root.canvas.pack(fill=BOTH, expand=1)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -21,3 +21,19 @@ class MazeWindow:
 
     def close(self):
         self.__is_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
