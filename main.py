5daf5a68d8ab58bdfa462afef57f745c0851b927
Cygran <apcanning90@gmail.com>
2025-02-15 22:19:31 +0000

    Add Maze class and refactor main to initialize maze with cells

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from window import MazeWindow, Point, Line
 from cell import Cell
+from maze import Maze
 
 def main():
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) // num_cols
+    cell_size_y = (screen_y - 2 * margin) // num_rows
     win = MazeWindow(800, 600)
-    
-    cell1 = Cell(win, 100, 100, 200, 200)
-    cell1.draw()
-    
-    cell2 = Cell(win, 200, 100, 300, 200)
-    cell2.has_right_wall = False
-    cell2.draw()
-
-    cell3 = Cell(win, 300, 100, 400, 200)
-    cell3.has_left_wall = False
-    cell3.has_right_wall = False
-    cell3.draw()
-    
-    cell4 = Cell(win, 400, 100, 500, 200)
-    cell4.has_left_wall = False
-    cell4.draw()
-    
-    move1 = cell2.draw_move(cell4)
-    move1_undone = cell2.draw_move(cell4, undo=True)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 if __name__ == '__main__':
