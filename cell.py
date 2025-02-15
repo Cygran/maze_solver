ec926498bc11b583759b024fc73062ff897c132a
Cygran <apcanning90@gmail.com>
2025-02-15 22:48:41 +0000

    Refactor Cell and Maze classes to use a unified Window parameter and enhance drawing methods; add unit tests for maze cell creation and entrance/exit functionality.

--- cell.py
+++ cell.py
@@ -1,29 +1,61 @@
 from window import Point, Line
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, window=None):
+    def __init__(self, win=None):
         self.has_left_wall = True
-        self.has_top_wall = True
         self.has_right_wall = True
+        self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = x1
-        self._y1 = y1
-        self._x2 = x2
-        self._y2 = y2
-        self._window = window
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
-    def draw(self):
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
-            self._window.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
-        color = "red" if not undo else "darkgray"
-        center = Point((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2)
-        to_center = Point((to_cell._x1 + to_cell._x2) // 2, (to_cell._y1 + to_cell._y2) // 2)
-        self._window.draw_line(Line(center, to_center), fill_color=color)
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from window import MazeWindow, Point, Line
-from cell import Cell
+from window import Window, Point, Line
 from maze import Maze
 
 def main():
@@ -8,10 +7,12 @@ def main():
     margin = 50
     screen_x = 800
     screen_y = 600
-    cell_size_x = (screen_x - 2 * margin) // num_cols
-    cell_size_y = (screen_y - 2 * margin) // num_rows
-    win = MazeWindow(800, 600)
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 
 if __name__ == '__main__':
