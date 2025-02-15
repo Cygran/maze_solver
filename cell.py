7d60c2cf7c38411345865c50905ba95fb9bcde7b
Cygran <apcanning90@gmail.com>
2025-02-15 21:19:47 +0000

    Implement Cell class and update main to draw cells with customizable walls

---
+++ cell.py
@@ -0,0 +1,23 @@
+from window import Point, Line
+
+class Cell:
+    def __init__(self, window, x1, y1, x2, y2):
+        self.has_left_wall = True
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._window = window
+
+    def draw(self):
+        if self.has_left_wall:
+            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+        if self.has_top_wall:
+            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        if self.has_right_wall:
+            self._window.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+        if self.has_bottom_wall:
+            self._window.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from window import MazeWindow, Point, Line
+from cell import Cell
 
 def main():
     win = MazeWindow(800, 600)
-    l = Line(Point(100, 100), Point(200, 200))
-    win.draw_line(l, "black")
+    
+    cell1 = Cell(win, 100, 100, 200, 200)
+    cell1.draw()
+    
+    cell2 = Cell(win, 200, 100, 300, 200)
+    cell2.has_right_wall = False
+    cell2.draw()
+
+    cell3 = Cell(win, 300, 100, 400, 200)
+    cell3.has_top_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw()
+    
+    cell4 = Cell(win, 400, 100, 500, 200)
+    cell4.has_left_wall = False
+    cell4.has_right_wall = False
+    cell4.draw()
     
     win.wait_for_close()
 
