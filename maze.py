6c71f8c1a20369379a6851e407c39a2ee7f9f5fb
Cygran <apcanning90@gmail.com>
2025-02-16 00:09:53 +0000

    Project Complete Implement maze solving functionality with recursive backtracking and add print statements for solution status

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 from cell import Cell
 import random
 import time
+from window import Window, Point, Line
 
 
 class Maze:
@@ -117,3 +118,42 @@ class Maze:
         for col in self._cells:
             for cell in col:
                 cell.visited = False
+    
+    def solve(self):
+        return self._solve_r(0,0)
+    
+    def _solve_r(self, i, j):
+        self._animate()
+        self._cells[i][j].visited = True
+        if (i, j) == (self._num_cols - 1, self._num_rows - 1):
+            return True
+
+        # left
+        if i > 0 and not self._cells[i - 1][j].visited and not self._cells[i][j].has_left_wall:
+            self._cells[i][j].draw_move(self._cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            self._cells[i][j].draw_move(self._cells[i - 1][j], True)  # undo
+
+        # right
+        if i < self._num_cols - 1 and not self._cells[i + 1][j].visited and not self._cells[i][j].has_right_wall:
+            self._cells[i][j].draw_move(self._cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            self._cells[i][j].draw_move(self._cells[i + 1][j], True)  # undo
+        
+        # up
+        if j > 0 and not self._cells[i][j - 1].visited and not self._cells[i][j].has_top_wall:
+            self._cells[i][j].draw_move(self._cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j - 1], True)  # undo
+        
+        # down
+        if j < self._num_rows - 1 and not self._cells[i][j + 1].visited and not self._cells[i][j].has_bottom_wall:
+            self._cells[i][j].draw_move(self._cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j + 1], True)  # undo
+        return False
+    
